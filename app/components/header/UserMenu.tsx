4eeb8728e585a5026c5b4193608885e34f632352
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-20 14:48:53 +0200

    🚧 | AB-8 | RENT-MODAL | Create rent-modal, release step-1 => Pick a category.

--- app/components/header/UserMenu.tsx
+++ app/components/header/UserMenu.tsx
@@ -8,6 +8,7 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import {signOut} from "next-auth/react";
 import {SafeUser} from "@/app/types";
+import useRentModal from "@/app/hooks/useRentModal";
 
 interface UserMenuProps {
     currentUser?: SafeUser | null
@@ -16,19 +17,26 @@ interface UserMenuProps {
 const UserMenu = ({currentUser}: UserMenuProps) => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
+    const rentModal = useRentModal();
     const [isOpen, setIsOpen] = React.useState(false);
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
     }, []);
 
+    const onRent = useCallback(() => {
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+        rentModal.onOpen();
+    }, [currentUser, loginModal, rentModal]);
+
     return (
         <div className="relative" >
             <div className="flex flex-row items-center gap-3" >
                 <div
                     className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-                    onClick={() => {
-                    }} >
+                    onClick={onRent} >
                     Airbnb your home
                 </div >
 
@@ -52,7 +60,7 @@ const UserMenu = ({currentUser}: UserMenuProps) => {
                                 <MenuItem onClick={() => console.log('click')} label="My favorites" />
                                 <MenuItem onClick={() => console.log('click')} label="My reservations" />
                                 <MenuItem onClick={() => console.log('click')} label="My properties" />
-                                <MenuItem onClick={() => console.log('click')} label="Airbnb my home" />
+                                <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
                                 <hr />
                                 <MenuItem onClick={() => signOut()} label="Logout" />
                             </>

a357191f9de560e5eb1371ab4b9def746755e308
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-20 23:42:11 +0200

    🚧 | AB-12 | RESERVATION | Create reservation UI

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,12 +1,25 @@
 'use client'
 
-import React, {useMemo} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {SafeListing, SafeUser} from "@/app/types";
 import {Reservation} from "@prisma/client";
 import {categories} from "@/app/constants/categories";
 import Container from "@/app/components/ui/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import {useRouter} from "next/navigation";
+import {differenceInCalendarDays, eachDayOfInterval} from "date-fns";
+import axios from "axios";
+import toast from "react-hot-toast";
+import ListingReservation from "@/app/components/listings/ListingReservation";
+import {Range} from "react-date-range";
+
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+};
 
 interface ListingClientProps {
     reservations?: Reservation[];
@@ -17,6 +30,69 @@ interface ListingClientProps {
 }
 
 const ListingClient: React.FC<ListingClientProps> = ({listing, reservations = [], currentUser}) => {
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const disabledDates = useMemo(() => {
+        let dates: Date[] = [];
+
+        reservations.forEach((reservation) => {
+            const range = eachDayOfInterval({
+                start: new Date(reservation.startDate),
+                end: new Date(reservation.endDate)
+            });
+
+            dates = [...dates, ...range];
+        });
+
+        return dates;
+    }, [reservations]);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(listing.price);
+    const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+
+    const onCreateReservation = useCallback(() => {
+            if (!currentUser) {
+                return loginModal.onOpen();
+            }
+            setIsLoading(true);
+
+            axios.post('/api/reservations', {
+                totalPrice,
+                startDate: dateRange.startDate,
+                endDate: dateRange.endDate,
+                listingId: listing?.id
+            })
+                .then(() => {
+                    toast.success('Listing reserved!');
+                    setDateRange(initialDateRange);
+                    // redirect to /trips
+                })
+                .catch(() => {
+                    toast.error('Something went wrong.');
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                })
+        },
+        [totalPrice, dateRange, listing?.id, router, currentUser, loginModal]);
+
+    // Update price when date range changes
+    useEffect(() => {
+        if (dateRange.startDate && dateRange.endDate) {
+            const dayCount = differenceInCalendarDays(
+                dateRange.endDate,
+                dateRange.startDate
+            );
+
+            if (dayCount && listing.price) {
+                setTotalPrice(dayCount * listing.price);
+            } else {
+                setTotalPrice(listing.price);
+            }
+        }
+    }, [dateRange, listing.price]);
 
     const category = useMemo(() => {
         return categories.find((items) =>
@@ -28,23 +104,31 @@ const ListingClient: React.FC<ListingClientProps> = ({listing, reservations = []
         <Container >
             <div className="max-w-screen-lg mx-auto" >
                 <div className="flex flex-col gap-6" >
-                    <ListingHead
-                        title={listing.title}
-                        imageSrc={listing.imageSrc}
-                        locationValue={listing.locationValue}
-                        id={listing.id}
-                        currentUser={currentUser}
+                    <ListingHead title={listing.title}
+                                 imageSrc={listing.imageSrc}
+                                 locationValue={listing.locationValue}
+                                 id={listing.id}
+                                 currentUser={currentUser}
                     />
                     <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6" >
-                        <ListingInfo
-                            user={listing.user}
-                            category={category}
-                            description={listing.description}
-                            roomCount={listing.roomCount}
-                            guestCount={listing.guestCount}
-                            bathroomCount={listing.bathroomCount}
-                            locationValue={listing.locationValue}
+                        <ListingInfo user={listing.user}
+                                     category={category}
+                                     description={listing.description}
+                                     roomCount={listing.roomCount}
+                                     guestCount={listing.guestCount}
+                                     bathroomCount={listing.bathroomCount}
+                                     locationValue={listing.locationValue}
                         />
+                        <div className="order-first mb-10 md:order-last md:col-span-3" >
+                            <ListingReservation price={listing.price}
+                                                totalPrice={totalPrice}
+                                                onChangeDate={(value) => setDateRange(value)}
+                                                dateRange={dateRange}
+                                                onSubmit={onCreateReservation}
+                                                disabled={isLoading}
+                                                disabledDates={disabledDates}
+                            />
+                        </div >
                     </div >
                 </div >
             </div >

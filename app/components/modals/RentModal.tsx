d100c520261429c0f5c99293be9a3e3940ce2210
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-20 18:52:52 +0200

    🚧 | AB-8 | RENT-MODAL | Set Upload Images with cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import {FieldValues, useForm} from "react-hook-form";
 import CountrySelect from "@/app/components/inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "@/app/components/inputs/Counter";
+import ImageUpload from "@/app/components/inputs/ImageUpload";
 
 enum STEPS {
     CATEGORY = 0,
@@ -48,6 +49,7 @@ const RentModal = () => {
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc');
 
     // Dynamic import to render the map only when needed (when the user is on the location step) and get zooming to work
     const Map = useMemo(() => dynamic(() => import('@/app/components/ui/Map'), {
@@ -137,6 +139,20 @@ const RentModal = () => {
                          value={bathroomCount}
                          title="Bathrooms"
                          subtitle="How many bathrooms do you have?"
+                />
+            </div >
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8" >
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place looks like!"
+                />
+                <ImageUpload onChange={(value) => setCustomValue('imageSrc', value)}
+                             value={imageSrc}
                 />
             </div >
         )
